fcdb2ec259a6672ef5cea0fd64ed92d5d8fa630f
CesarClavijo01 <cesar@Cesar>
2024-03-08 20:51:42 -0600

    completed workshop

---
+++ index.js
@@ -0,0 +1,33 @@
+// tests for multiplication function
+
+//expect (a, b) to return a number
+//expect (2, 5) to return 10
+//expect string input to return error
+//expect (a, b, c...) to return the product of the first two olny
+//expect no input to be an error
+//expect (-1, 2) to return -2
+
+// test for contactOdds function
+
+//expect ([1,2,3,4,5] [4,2,6,7,1,3,9,0]) to return ([1,3,5,7,9])
+//expect string input to return error
+//expect multiples of the same number to return the number once
+//expect negative numbers to be returned as well
+//expect more than two arrays to return error
+//expect one array to be returned on its own
+//expect no input to return error
+
+//tests for cart checkout feature
+
+//when cart is opened user is shown all items in the cart
+//allow user to increase or decrease the amount of each item they want to purchase
+//when check out is selected user is shown all items, prices and total price of the cart
+//when checking out user can select items to leave in the cart to purchase later
+//if cart is empty when user goes to check out page a warning appears saying the cart is empty and gives recommendations on items to purchase
+//if checking out as guest, a prompt appears to recommend log in, sign up or continue as guest
+//if user has an account a password entry would be required to prevent fraud
+//if wrong password is entered wrong password warning is prompted
+//during checkout, user is prompted to select shipping and payment options
+//if no payment option is selected checkout button is disabled
+//if no shipping option is selected checkout button is disabled
+//once checkout is complete send order confirmation email with shipping info
